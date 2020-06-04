ecb9924e7b08c6a85b4218649deea7246f50c0a5
cjinsteng <fpig503@gmail.com>
2020-06-04 21:49:42 +0900

    git

---
+++ app/src/main/java/com/cjent/note/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.cjent.note;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
