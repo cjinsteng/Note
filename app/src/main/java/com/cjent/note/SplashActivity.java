014c5a710928fe61db8fe40a5cde3886f4f7b477
cjinsteng <fpig503@gmail.com>
2020-06-06 18:15:37 +0900

    ADD : NoteListActivity.class is add.
    FireBase Program Connect!

--- app/src/main/java/com/cjent/note/SplashActivity.java
+++ app/src/main/java/com/cjent/note/SplashActivity.java
@@ -2,7 +2,9 @@ package com.cjent.note;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+//        splashActivity 실행후 3초후에 NoteActivity 실행한다.
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), NoteListActivity.class);
+                startActivity(intent);
+            }
+
+        }, 3000);
     }
 }
